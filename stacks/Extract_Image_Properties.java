a417ca553992de1a9db3bbda1c824c494b005d02
Mark Longair <mhl@pobox.com>
2008-04-15 14:51:03 +0100

    Added a PlugIn to be used by the Job Server to extra metadata about files.

---
+++ stacks/Extract_Image_Properties.java
@@ -0,0 +1,243 @@
+/* -*- mode: java; c-basic-offset: 8; indent-tabs-mode: t; tab-width: 8 -*- */
+
+/* Copyright 2006, 2007 Mark Longair */
+
+/*
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License as
+    published by the Free Software Foundation; either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package stacks;
+
+import util.BatchOpener;
+
+import ij.IJ;
+import ij.ImagePlus;
+import ij.ImageStack;
+import ij.gui.ImageWindow;
+import ij.Macro;
+import ij.LookUpTable;
+import ij.plugin.PlugIn;
+import ij.measure.Calibration;
+
+import java.util.Hashtable;
+import java.util.Enumeration;
+import java.io.*;
+
+import amira.AmiraParameters;
+
+import org.jvyaml.YAML;
+import org.jvyaml.YAMLConfig;
+
+public class Extract_Image_Properties implements PlugIn {
+
+	static String typeToString(int type) {
+		switch(type) {
+		case ImagePlus.GRAY8:
+			return "GRAY8";
+		case ImagePlus.GRAY16:
+			return "GRAY16";
+		case ImagePlus.GRAY32:
+			return "GRAY32";
+		case ImagePlus.COLOR_256:
+			return "COLOR_256";
+		case ImagePlus.COLOR_RGB:
+			return "COLOR_RGB";
+		default:
+			return "Unknown";
+		}
+	}
+
+/*
+	static String escapeStringForYAML( String s ) {
+                String result = s.replaceAll("\\\\","\\\\\\\\");
+                result = result.replaceAll("\\\"","\\\\\"");
+                return result;		
+	}
+
+
+	static String objectToYAML( Object o ) {
+		objectToYAML( o, 0 );
+	}
+
+	static String spaces( int n ) {
+		char [] s=new char[n];
+		for( int i = 0; i < n; ++i  )
+			s[i] = ' ';
+		return new String(s);
+	}
+
+	static String objectToYAML( Object o, int leadingSpaces ) {
+
+		// FIXME: This actually doesn't work in general.  We
+		// should just replace this with something like
+		// 
+
+		if( o instanceof String ) {
+			String s = (String)o;	
+			return "\""+escapeStringForYAML(key)+"\"";
+		} else if( o instanceof Integer ) {
+			Integer i = (Integer)o;
+			return ""+i;
+		} else if( o instanceof Long ) {
+			Long l = (Long)o;
+			return ""+l;
+		} else if( o instanceof Double ) {
+			Double d = (Double)o;
+			return ""+d;
+		} else if( o instanceof Float ) {
+			Float d = (Float)o;
+			return ""+d;
+		} else if( o instanceof Hashtable<String,Object> ) {
+			Hashtable<String,Object> h = (Hashtable<String,Object>)o;
+			String result = "";
+			for( Enumeration ke=properties.keys(); ke.hasMoreElements(); ) {
+				String key = (String)ke.nextElement();
+				String value = objectToYAML( h.get(key), 0 );
+				result += objectToYAML(key,0)
+			}
+		
+			result += "}\n":
+		}
+
+		return "";
+	}
+*/
+
+	boolean client = false;
+	
+	public void run( String pluginArguments ) {
+		
+		String realArguments = null;
+		String macroArguments = Macro.getOptions();
+		
+		if( (macroArguments == null) || (macroArguments.equals("")) ) {
+			
+			if( (pluginArguments == null) || (pluginArguments.equals("")) ) {
+				throw new RuntimeException("No parameters supplied either as macro options or a plugin argument.");
+			} else {
+				realArguments = pluginArguments;
+			}
+			
+		} else { 
+			realArguments = macroArguments;
+		}
+		
+		String filename = Macro.getValue(
+			realArguments,
+			"filename",
+			"");
+		
+		if( filename.equals("") )
+			throw new RuntimeException("No macro parameter 'filename' supplied");
+		
+		String destinationDirectory = Macro.getValue(
+			macroArguments,
+			"directory",
+			"");
+
+		
+		if( destinationDirectory.equals("") )
+			throw new RuntimeException("No macro parameter 'directory' supplied");
+		
+		String clientString = Macro.getValue(
+			macroArguments,
+			"client",
+			null );
+		if( clientString != null )
+			client = true;
+		
+		Hashtable<String,Object> properties=new Hashtable<String,Object>();
+		
+		String loaderUsed = "foo";
+		
+		BatchOpener.ChannelsAndLoader cal = null;
+		try {
+			cal = BatchOpener.openToChannelsAndLoader( filename );
+		} catch( BatchOpener.ImageLoaderException e ) {
+		}
+
+		if( cal == null )
+			throw new RuntimeException("Couldn't open the file: "+filename);
+
+		properties.put( "loader", cal.loaderUsed );
+		
+		ImagePlus [] imps = cal.channels;
+		
+		properties.put( "channels",  imps.length);
+		
+		int width  = imps[0].getWidth();
+		int height = imps[0].getHeight();
+		int depth  = imps[0].getStackSize();
+		
+		properties.put( "width",     width);
+		properties.put( "height",    height);
+		properties.put( "depth",     depth);
+		properties.put( "imagej-type", typeToString(imps[0].getType()) );
+
+		Calibration c = imps[0].getCalibration();
+
+		if( c != null ) {
+			properties.put( "sample-spacing-x:",      c.pixelWidth );
+			properties.put( "sample-spacing-y:",      c.pixelHeight );
+			properties.put( "sample-spacing-z:",      c.pixelDepth );
+			properties.put( "sample-spacing-unit:",   c.getUnit() );
+			properties.put( "sample-spacing-units: ", c.getUnits() );
+		}
+
+		// Now write out the properties we've discovered as YAML:
+
+		File outputDirectoryFile = new File(destinationDirectory);
+		
+		if( ! outputDirectoryFile.exists() ) {
+			throw new RuntimeException("The output directory ('"+destinationDirectory+"') didn't exist");
+		}
+		
+		String outputFilename = outputDirectoryFile.getAbsolutePath() + File.separator + "properties-generic";
+
+		if( loaderUsed.equals("LSM_Toolbox") ) {
+			// Then we should write out some additional information:
+			String outputFilenameLSM = outputDirectoryFile.getAbsolutePath() + File.separator + "properties-LSM";
+			// FIXME: implement this
+
+		}
+	       
+		PrintWriter pw = null;
+		try {
+			pw = new PrintWriter(new OutputStreamWriter(new FileOutputStream(outputFilename),"UTF-8"));
+		} catch( Exception e ) {
+			throw new RuntimeException("Couldn't open the file: '"+outputFilename+"' for writing; exception was: "+e);
+		}
+
+		YAMLConfig y=YAML.config();
+		String d=YAML.dump(properties,y);
+
+		pw.print(d);
+		
+
+/*
+		for( Enumeration ke=properties.keys(); ke.hasMoreElements(); ) {
+			String key = (String)ke.nextElement();
+			String value = ""+properties.get(key);
+
+		}
+*/
+		pw.close();
+
+		// close all ImagePlus objects
+		
+		for( int i = 0; i < imps.length; ++i ) {
+			imps[i].close();
+		}
+	}
+}
