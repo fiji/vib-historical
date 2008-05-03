34d5fe25f55ef4fdbf2cbd9df554a031b7c44b7a
Mark Longair <mhl@pobox.com>
2008-05-03 10:15:37 +0100

    Just use the normal ImageJA and do some checks on UID, branch when running

--- run-server.sh
+++ run-server.sh
@@ -1,10 +1,22 @@
 #!/bin/sh
 
+if ! [ `whoami` = 'www-data' ]
+then
+	echo The server must be run as the www-data user.
+	exit -1
+fi
+
+if ! [ `( cd ../ImageJA && git-branch | egrep '^\*' | sed 's/\* //' )` = xvfb-server ]
+then
+	echo The respository ../ImageJA must be at xvfb-server
+	exit -1
+fi
+
 set -e
 
 MEM=1024m
 
-xvfb-run java -Xmx$MEM -Dplugins.dir=. -jar ../ImageJ/ij.jar -eval 'run("Job Server","");'
+xvfb-run java -Xmx$MEM -Dplugins.dir=. -jar ../ImageJA/ij.jar -eval 'run("Job Server","");'
 
 
 
