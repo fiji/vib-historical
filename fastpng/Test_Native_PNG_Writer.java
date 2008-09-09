bb6a95373944ea074290becbf0aec41d8dbbcec3
Mark Longair <mhl@pobox.com>
2008-09-09 14:04:54 +0100

    Move the test code into another file

--- fastpng/Test_Native_PNG_Writer.java
+++ fastpng/Test_Native_PNG_Writer.java
@@ -25,14 +25,34 @@
 
 package fastpng;
 
-class Native_PNG_Writer {
+import ij.ImagePlus;
+import ij.ImageStack;
 
-	public native boolean write8BitPNG( byte [] pixelData, int width, int height, byte [] reds, byte [] greens, byte [] blues, String outputFilename );
+import util.BatchOpener;
 
-	public native boolean writeFullColourPNG( int [] pixelData, int width, int height, String outputFilename );
+public class Test_Native_PNG_Writer {
 
-	static {
-		System.loadLibrary("fastpng");
+	public static void main( String [] arguments ) {
+
+		ImagePlus [] images = BatchOpener.open("test-images/tidied-mhl-62yxUAS-lacZ0-reduced.tif");
+		ImageStack stack = images[0].getStack();
+
+		byte [] pixelData = (byte[])stack.getPixels(stack.getSize()/2);
+
+		Native_PNG_Writer writer = new Native_PNG_Writer();
+
+		writer.write8BitPNG( pixelData, stack.getWidth(), stack.getHeight(), null, null, null, "test-grey.png");
+
+		byte [] reds   = new byte[256];
+		byte [] greens = new byte[256];
+		byte [] blues  = new byte[256];
+
+		for( int i = 0; i < 256; ++i ) {
+			reds[i] = blues[i] = (byte)i;
+			greens[i] = 0;
+		}
+
+		writer.write8BitPNG( pixelData, stack.getWidth(), stack.getHeight(), reds, greens, blues, "test-colour.png");
+
 	}
-
 }
