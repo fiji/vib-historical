302d88611bd3c158f8304c30a207421410ea706e
Benjamin Schmid <bene.schmid@gmail.com>
2010-03-23 16:56:55 +0100

    Image3DUniverse: Avoid update problem.

--- ij3d/Image3DUniverse.java
+++ ij3d/Image3DUniverse.java
@@ -1241,7 +1241,7 @@ public class Image3DUniverse extends DefaultAnimatableUniverse {
 				}
 				univ.fireContentAdded(c);
 				univ.addUniverseListener(c);
-// 				univ.waitForNextFrame();
+				univ.waitForNextFrame();
 				univ.fireTransformationUpdated();
 				return c;
 			}
